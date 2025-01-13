6203c2d49db1e663a7eaf614434d3ac4fab53f9a
Apfirebolt <aspper20@gmail.com>
2025-01-14 04:02:17 +0530

    feat: added scss

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,14 @@
 import { AppBar, Toolbar, Typography } from '@mui/material';
+import './footer.scss';
 
 const Footer = () => {
     return (
         <AppBar position="static" style={{ width: '100%', marginTop: '20px' }} background="secondary">
+            <footer className="footer">
+                <p>
+                    © 2025 React Games Database. All rights reserved
+                </p>
+            </footer>
             <Toolbar>
                 <Typography variant="body1" color="inherit">
                     © 2025 React Games Database. All rights reserved.
